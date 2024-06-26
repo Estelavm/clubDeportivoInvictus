5e18f1424031f1b19e882cf50ce069af92a5fc6d
Estela <estela.vm@hotmail.com.ar>
2024-06-26 18:49:38 -0300

    Terminada la activity de listar morosos

--- app/src/main/java/com/example/platzi/DCMorosos.kt
+++ app/src/main/java/com/example/platzi/DCMorosos.kt
@@ -1,6 +1,8 @@
 package com.example.platzi
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.platzi.model.Delinquent
@@ -10,6 +12,7 @@ class MorososActivity : AppCompatActivity() {
 
     private lateinit var listViewMorosos: ListView
     private lateinit var dbHelper: DatabaseHelper
+    private lateinit var btnAtras: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,11 +20,18 @@ class MorososActivity : AppCompatActivity() {
 
         listViewMorosos = findViewById(R.id.listViewMorosos)
         dbHelper = DatabaseHelper.getInstance(this)
+        btnAtras = findViewById(R.id.btn_atras)
 
         val morososList = getMorosos() // Obtener la lista de socios morosos
 
         val adapter = DelinquentAdapter(this, morososList)
         listViewMorosos.adapter = adapter
+
+        btnAtras.setOnClickListener {
+            val intent = Intent(this, DAPagoSocio::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     private fun getMorosos(): List<Delinquent> {
@@ -63,11 +73,12 @@ class MorososActivity : AppCompatActivity() {
     private fun getFictitiousDelinquents(): List<Delinquent> {
         // Aquí defines usuarios ficticios con cuotas mensuales ficticias
         val fictitiousUsers = listOf(
-            Delinquent("Juan", "Pérez", "DNI", "12345678", 3, 1500.0),
-            Delinquent("María", "Gómez", "CI", "87654321", 2, 1000.0),
+            Delinquent("Juan", "Pérez", "DNI", "12345678", 3, 15000.0),
+            Delinquent("María", "Gómez", "CI", "87654321", 2, 10000.0),
             Delinquent("Pedro", "Martínez", "DNI", "56789123", 1, 5000.0)
             // Agrega más usuarios ficticios según sea necesario
         )
         return fictitiousUsers
     }
+
 }
